96d4b7482fbfb50da2b9a3e41f70017ad12642a1
saki <sakihet@gmail.com>
2017-04-22 02:20:32 +0900

    init

---
+++ spec/backlog-key-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import BacklogKeyView from '../lib/backlog-key-view';
+
+describe('BacklogKeyView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
